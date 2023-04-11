aa571e20c0a3d3e64c736f156e9dafa017b869a3
Shantayya Swami <shantayyaswami4@gmail.com>
2023-04-11 17:29:54 +0100

    Add files via upload

--- SQL_Challenge-2.sql
+++ SQL_Challenge-2.sql
@@ -70,4 +70,13 @@ on p.id = test1.caller_id) test2
 on cn.country_code = test2.pnum 
 where test2.avg_call_duration > (select round(avg(duration),2) as global_avg_call from calls1) group by cn.name;
 
--- Q.56
+-- Q.56 Write an SQL query to report the device that is first logged in for each player.
+create table activity1(player_id int, device_id int, event_date date, games_played int); 
+select * from activity1;
+insert into activity1 values(1,2,'2016-03-01',5),(1,2,'2016-05-02',6),(2,3,'2017-06-25',1),(3,1,'2016-03-02',0),(3,4,'2018-07-03',5);
+
+select player_id, device_id from activity1 where event_date in (select min(event_date) from activity1 group by player_id);
+-- or
+select player_id,device_id from (
+select player_id,device_id,row_number() over (partition by player_id order by event_date) as rownum from activity1) test where rownum = 1;
+
